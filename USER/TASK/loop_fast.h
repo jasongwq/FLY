f645c862e67a7880582a688cea6ebe094f3a0fd3
jasongwq <jasongwq@gmail.com>
2014-11-15 12:27:37 +0800

    'tmp'

--- USER/TASK/loop_fast.h
+++ USER/TASK/loop_fast.h
@@ -21,6 +21,31 @@ int loop_fast(void)//500hz
     LoopX(2);
     _EE
 }
+//int task_fast(void)//500hz
+//{
+//extern S_INT16_XYZ Acc, Average_Acc, Gyr, Mag;
+
+//    _SS
+//		while (1)
+//    {
+//    if (flag_ACC)
+//    {
+//		    WaitX(2);
+//        IMUupdate(&Gyr, &Average_Acc, &Att_Angle);//222us
+//        Prepare_Data2(&Att_Angle);//24us
+//        //        //Control(&Att_Angle, &Gyr, &Rc_D, &RC_Control);//17us
+//        Balance(&Att_Angle, &Gyr, &Acc, &Rc_D, &RC_Control); //17us
+//        {
+//            static u32 currenttime = 0;
+//            u32 lasttime = 0;
+//            lasttime = currenttime;
+//            currenttime = SysTick_Clock();
+//            Sys_Printf(Printf_USART, "\r\n %d", currenttime - lasttime);
+//        }
+//    }
+//    }
+//    _EE
+//}
 int task_fast(void)
 {
 extern S_INT16_XYZ Acc, Average_Acc, Gyr, Mag;
