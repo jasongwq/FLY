b646cc945fe44acdadc21ff626c940f4cc137808
jasongwq <jasongwq@gmail.com>
2014-11-15 12:54:37 +0800

    'tmp'

--- USER/main.c
+++ USER/main.c
@@ -63,8 +63,8 @@ int main(void)
 
         //       RunLoop(loop_led,1);
 
-RunTaskA(task_6050, 0);
 RunTaskA(task_fast, 1);
+RunTaskA(task_6050, 0);
 //RunLoop(loop_fast, 1);
 #if VERSIONS ==1
         // RunTaskA(task_cap_rc, 3);
