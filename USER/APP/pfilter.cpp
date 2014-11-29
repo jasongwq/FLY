5cbfd688547c99cdbbd7ea0c0e470d838dfcecb0
jasongwq <jasongwq@gmail.com>
2014-11-29 21:12:22 +0800

    cpp

---
+++ USER/APP/pfilter.cpp
@@ -0,0 +1,27 @@
+#include "sys.h"
+#include "filter.h"
+
+u8 slide_filtering(SLIDE_FILTERING8 dat)
+{
+    dat.sum = dat.sum - dat.pbuf[dat.cnt] + dat.data;
+    dat.pbuf[dat.cnt] = dat.data;
+    dat.cnt++; if (dat.cnt == dat.num) dat.cnt = 0;
+    return dat.sum / dat.num;
+}
+u16 slide_filtering(SLIDE_FILTERING16 dat)
+{
+    dat.sum = dat.sum - dat.pbuf[dat.cnt] + dat.data;
+    dat.pbuf[dat.cnt] = dat.data;
+    dat.cnt++; if (dat.cnt == dat.num) dat.cnt = 0;
+    return dat.sum / dat.num;
+}
+float slide_filtering(SLIDE_FILTERINGF dat)
+{
+    dat.sum = dat.sum - dat.pbuf[dat.cnt] + dat.data;
+    dat.pbuf[dat.cnt] = dat.data;
+    dat.cnt++; if (dat.cnt == dat.num) dat.cnt = 0;
+    return dat.sum / dat.num;
+}
+
+
+
