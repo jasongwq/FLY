40de252c968f869ba3519ff0c08aeb400d911b5a
jasongwq <jasongwq@gmail.com>
2014-12-02 22:32:48 +0800

    'tmp'

--- USER/main.cpp
+++ USER/main.cpp
@@ -58,18 +58,18 @@ int main(void)
     /***总循环***/
     while (1)
     {
-//        RunTaskA(task_6050, 0);
-//        RunTaskA(task_fast, 1);
-//#if VERSIONS ==1
-//#elif VERSIONS ==2
-//        RunTaskA(task_cap_rc, 2);
-//#endif
-//				
+        RunTaskA(task_6050, 0);
+        RunTaskA(task_fast, 1);
+#if VERSIONS ==1
+#elif VERSIONS ==2
+        RunTaskA(task_cap_rc, 2);
+#endif
+				
 //        RunTaskA(task_ultrasonic, 3);
 //				RunTaskA(task_bmp085, 4);
 //        //        RunTaskA(task_hmc5883l, 6);
 //        
-//        RunTaskA(task_pwm_ex, 5);
+        RunTaskA(task_pwm_ex, 5);
         RunTaskA(task_led, 6);
     }
 }
