03c58eb0e86ec3ba70f4b105be603f1ec8508216
jasongwq <jasongwq@gmail.com>
2014-12-02 16:52:58 +0800

    'tmp'

--- USER/TASK/task_bmp085.h
+++ USER/TASK/task_bmp085.h
@@ -49,8 +49,8 @@ int task_bmp085(void)
             BMP085_pressure_start();
             WaitX(5);
 						temp_up=BMP085_pressure_get();
-						Calculate(temp_ut,temp_up,&bmp085);       // 计算温度气压和高度
-//            BMP085_Calculate(temp_up, &bmp085);
+						//Calculate(temp_ut,temp_up,&bmp085);       // 计算温度气压和高度
+            BMP085_Calculate(temp_up, &bmp085);
 //						Alt_bmp=(u32)(bmp085.altitude*100);
 						//WaitX(100);
             BMP085_Printf(&bmp085);
