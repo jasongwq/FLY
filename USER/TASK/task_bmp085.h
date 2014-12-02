30d8142c50c1b83eb4abe124c7abadd7b1d62672
jasongwq <jasongwq@gmail.com>
2014-12-02 16:52:12 +0800

    'tmp'

--- USER/TASK/task_bmp085.h
+++ USER/TASK/task_bmp085.h
@@ -18,43 +18,43 @@ int task_bmp085(void)
 
     while (1)
     {
-        WaitX(100);
-//        BMP085_Read(&bmp085);
-		BMP085_Printf(&bmp085);
-    //temp_ut = bmp085ReadTemp();      // 读取温度
-		BMP085_temperature_start();
-        WaitX(5);
-        temp_ut = BMP085_temperature_get();
-				           BMP085_pressure_start();
-            WaitX(5);
-						temp_up=BMP085_pressure_get();
-//   //temp_up = bmp085ReadPressure();  // 读取压强
-//	 {
-//    //int32_t pressure = 0;
-////    I2c_Write(BMP085_Addr, 0xF4, 0x34);
-////    delay_ms(5); // max time is 4.5ms
-//    uint8_t tmp[2];
-//    I2c_ReadBuffer(BMP085_Addr, 0xF6, 2, tmp); //读出2个数据
-//    temp_up = (int16_t)( (tmp[0] << 8) + tmp[1]  );
-//    temp_up &= 0x0000FFFF; //不能去掉 原因未知
-//    //return pressure;
-//}
-    Calculate(temp_ut, temp_up,  &bmp085); //计算结果放入结构体
-//        BMP085_temperature_start();
+//        //WaitX(100);
+////        BMP085_Read(&bmp085);
+//		BMP085_Printf(&bmp085);
+//    //temp_ut = bmp085ReadTemp();      // 读取温度
+//		BMP085_temperature_start();
 //        WaitX(5);
 //        temp_ut = BMP085_temperature_get();
-//        static int i;
-//        for (i = 0; i < 1; i++)
-//        {
-//            BMP085_pressure_start();
+//				           BMP085_pressure_start();
 //            WaitX(5);
 //						temp_up=BMP085_pressure_get();
-//						Calculate(temp_ut,temp_up,&bmp085);       // 计算温度气压和高度
-////            BMP085_Calculate(temp_up, &bmp085);
-////						Alt_bmp=(u32)(bmp085.altitude*100);
-//						WaitX(100);
-//            BMP085_Printf(&bmp085);
-//        }
+////   //temp_up = bmp085ReadPressure();  // 读取压强
+////	 {
+////    //int32_t pressure = 0;
+//////    I2c_Write(BMP085_Addr, 0xF4, 0x34);
+//////    delay_ms(5); // max time is 4.5ms
+////    uint8_t tmp[2];
+////    I2c_ReadBuffer(BMP085_Addr, 0xF6, 2, tmp); //读出2个数据
+////    temp_up = (int16_t)( (tmp[0] << 8) + tmp[1]  );
+////    temp_up &= 0x0000FFFF; //不能去掉 原因未知
+////    //return pressure;
+////}
+//    Calculate(temp_ut, temp_up,  &bmp085); //计算结果放入结构体
+        BMP085_temperature_start();
+        WaitX(5);
+        temp_ut = BMP085_temperature_get();
+        static int i;
+        for (i = 0; i < 1; i++)
+        {
+            BMP085_pressure_start();
+            WaitX(5);
+						temp_up=BMP085_pressure_get();
+						Calculate(temp_ut,temp_up,&bmp085);       // 计算温度气压和高度
+//            BMP085_Calculate(temp_up, &bmp085);
+//						Alt_bmp=(u32)(bmp085.altitude*100);
+						//WaitX(100);
+            BMP085_Printf(&bmp085);
+        }
     }
     _EE
 }
