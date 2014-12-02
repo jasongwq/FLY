765a77dfa98494e80f441a50d351d3c693ed82bb
jasongwq <jasongwq@gmail.com>
2014-12-02 16:28:20 +0800

    'tmp'

--- USER/TASK/task_bmp085.h
+++ USER/TASK/task_bmp085.h
@@ -20,7 +20,7 @@ int task_bmp085(void)
 //        BMP085_Read(&bmp085);
 //				BMP085_Printf(&bmp085);
         BMP085_temperature_start();
-        WaitX(3);
+        WaitX(5);
         temp_ut = BMP085_temperature_get();
         static int i;
         for (i = 0; i < 40; i++)
