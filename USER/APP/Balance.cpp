62771b3c0e0e8a1ef6469631aff781948b821110
jasongwq <jasongwq@gmail.com>
2014-12-03 20:29:18 +0800

    'tmp'

--- USER/APP/Balance.cpp
+++ USER/APP/Balance.cpp
@@ -195,12 +195,10 @@ void Balance(T_float_angle *att_in, S_INT16_XYZ *gyr_in, S_INT16_XYZ *acc_in, T_
     Yaw_Control();
     Rol_Control();
     Pit_Control();
-    if (rc_in->THROTTLE > RC_FUN_MIN)
-    {
         //u32 ALT_Set = (rc_in->THROTTLE - RC_FUN_MIN);
         u32 ALT_Set = 500; // (rc_in->AUX2 - 1000);
         ALT_Control(ALT_Set);
-//        
+
 //        static s16 alt_tmp[1000] = {0};
 //        static SLIDE_FILTERING16 alt_control = {alt_tmp, 0, sizeof(alt_tmp) / sizeof(alt_tmp[0]), 0, 0};
 //        alt_control.data = (s16)Throttle_IN;//(s16)PID_ALT.OUT;
@@ -254,7 +252,6 @@ void Balance(T_float_angle *att_in, S_INT16_XYZ *gyr_in, S_INT16_XYZ *acc_in, T_
         {
             Throttle_OUT += PID_ALT.OUT;
         }
-    }
     /*****************************************************
     /CONTROL
     *****************************************************/
